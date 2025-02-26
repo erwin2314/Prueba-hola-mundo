01f46858c4bb393f93adef41d59b0a8e39035bae
Charlie <carlosjjp2016@gmail.com>
2025-02-25 18:02:31 -0600

    inicio de tipos de datos

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
@@ -17,6 +18,8 @@ int main()
     printf ("1\t2\t3\t4\t5\n6\t7\t8\t9\t10");
     printf ("\n\"esto es en serio\"");
     */
+
+    /*
     int x;// declaracion
     x = 1; // inicializacion
     int y = 2; 
@@ -27,5 +30,12 @@ int main()
     printf ("\n%s",nombre);//%S se utiliza para strings
     printf ("\n%c", letra);//%c se utiliza para caracteres
     printf ("\n%f",flotante); //%f se utiliza para floats
+    */
+   //especificadores de formato %0.15x para representar mas numeros
+    char a = 'C'; //caracter unico %c
+    char b[] = 'Bro'; //array de caracteres %s
+    float c = 3.141592; //4 bytes (32 bits de precision) 6 - 7 digitos %f
+    double d = 3.141592653589793; //8 Bytes (64 bits de precision) 15 - 16 digitos %lf
+    bool e = true; //1 byte (verdadero o falso) %d
     return 0;
 }
